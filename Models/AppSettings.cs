437d7d8289bdd9336f4ad83d58766b4a517cd979
Hemant <hemant.shelar@gmail.com>
2024-01-06 19:38:21 +1100

    Working DI.

---
+++ Models/AppSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core_6_api_console_client.Models
+{
+	public class AppSettings
+	{
+        public WFAPIDetails WFAPIDetails { get; set; }
+
+    }
+
+	public class WFAPIDetails
+	{
+        public string WetherForecastApiEndPoint { get; set; }
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,43 @@
-﻿namespace core_6_api_console_client;
+﻿using core_6_api_console_client.IServices;
+using core_6_api_console_client.Models;
+using core_6_api_console_client.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace core_6_api_console_client;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        #region Load configuration
+        IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appSettings.json")
+            .Build();
+		#endregion
+
+
+		var serviceCollection = new ServiceCollection();
+        serviceCollection.AddTransient<IWeatherForecastProvider,WeatherForecastProvider>();
+        serviceCollection.AddLogging();
+        serviceCollection.AddOptions();
+        //serviceCollection.Configure<WFAPIDetails>(options => config.GetSection("WFAPIDetails"));
+        
+        //This is important in order to inject dependency to WeatherForecastProvider constructor.
+        //Initially could not locate Bind extension method but after installing package
+        //Microsoft.Extensions.Hosting  things wokred magically.
+        serviceCollection.AddOptions<WFAPIDetails>().Bind(config.GetSection("WFAPIDetails"));
+
+
+        var sp = serviceCollection.BuildServiceProvider();
+
+        IWeatherForecastProvider wfp = sp.GetService<IWeatherForecastProvider>();
+
+        var t = config.GetSection("WFAPIDetails:WetherForecastApiEndPoint");
+        Console.WriteLine("OK");
+
     }
 }
